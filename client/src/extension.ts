aa3e1136d4972414e8ef9b081dd5ee412660c522
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2023-12-07 14:25:33 +0100

    Renaming extension: Language Server Modelica
      - Adding CI for server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -43,8 +43,8 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    'languageServerModelica',
+    'Language Server Modelica',
     serverOptions,
     clientOptions
   );
